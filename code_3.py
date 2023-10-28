b84af8cbd6274341d7944b1bd30661b71bd5f305
Mila <m9234434@gmail.com>
2023-10-28 12:35:31 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,16 @@
+}
+    for (let i = 0; i < 10; i++) {
+}
+    });
+    return data;
+    const data = generateRandomData();
+    data.forEach(item => {
+    const data = [];
+    }
+
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
+function main() {
+
+main();
