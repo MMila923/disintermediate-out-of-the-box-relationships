8f1890a5a68e1f22020f2b12c04156aa68161adf
Mila <m9234434@gmail.com>
2023-10-31 00:10:03 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,55 @@
+}
+    });
+    for (let i = 0; i < 10; i++) {
+    return data;
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = [];
+
+    const data = generateRandomData();
+        console.log("Random Number: " + item);
+main();
+}
+    data.forEach(item => {
+
+
+    }
+function generateRandomData() {    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+import random
+        print(f"Random Number: {item}")
+def main():
+
+def generate_random_data():
+    main()
+    data = generate_random_data()
+
+
+    return data
+
+if __name__ == "__main__":        }
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
+        int[] data = new int[10];
+}
+        for (int i = 0; i < 10; i++) {
+
+public class RandomDataGenerator {
+    }
+
+        }
+    public static void main(String[] args) {
+            data[i] = (int) (Math.random() * 100) + 1;    main()
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+def generate_random_data():
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+def main():
+    return data
+    for item in data:
+
+import random
